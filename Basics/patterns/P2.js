524633f4d7c9f364b1febff11790f82bad5ed0c3
Nihal <nihal301298@gmail.com>
2025-01-01 22:32:04 +0530

    cpp eg

--- Basics/patterns/P2.js
+++ Basics/patterns/P2.js
@@ -21,3 +21,29 @@ const N = 5
 const Sol= new Solution()
 
 Sol.pattern2(N)
+
+// cpp eg 
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution {
+//     public:
+
+//     static void pattern2(int n){
+//         for (int i = 0 ; i <n ; i++){
+//             for (int j = 0 ;j<= i ,j++){
+//                 cout<<'*',
+//             }
+//          cout << endl;
+//         }
+//     }
+// }
+// int main() {
+//     int N = 5;
+    
+//     //Create an instance of Solution class
+//     Solution sol;
+    
+//     sol.pattern2(N);
+// }
