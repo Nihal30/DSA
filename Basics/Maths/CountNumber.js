83dc609723955e0dd0f9770f9fd97a5968c97975
Nihal <nihal301298@gmail.com>
2025-01-02 22:59:49 +0530

    JS count digits

--- Basics/Maths/CountNumber.js
+++ Basics/Maths/CountNumber.js
@@ -13,25 +13,44 @@
 
 // Solution --------------
 
-class Count {
-  countNumber(n) {
-    // if number is 0 the return 1
-    if (n === 0) return 1;
-    // variable to store the output
-    let count = 0;
-    while (n > 0) {
-      let lastDigit = n % 10;
-      count = count + 1;
+// class Count {
+//   countNumber(n) {
+//     // if number is 0 the return 1
+//     if (n === 0) return 1;
+//     // variable to store the output
+//     let count = 0;
+//     while (n > 0) {
+//       let lastDigit = n % 10;
+//       count = count + 1;
 
-      n = Math.floor(n / 10); // for the next iteration
-    }
-    return count;
-  }
-}
+//       n = Math.floor(n / 10); // for the next iteration
+//     }
+//     return count;
+//   }
+// }
 
-let n = 12345;
-const C = new Count();
+// let n = 12345;
+// const C = new Count();
 
-let ans = C.countNumber(n);
+// let ans = C.countNumber(n);
 
-console.log("ans", ans); // output => 5
+// console.log("ans", ans); // output => 5
+
+// Again doing it 
+
+
+// class DigitCount {
+//   countNumber(n){
+
+//     if(n===0) return 1
+//     let count = 0
+//     while (n>0){
+//       let lastDigit = n%10;
+//       count = count +1;
+//       n = Math.floor(n/10)
+
+//     }
+    
+//     return count; 
+//   }
+// }
