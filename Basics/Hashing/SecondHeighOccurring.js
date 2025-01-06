46c7255fe3a5b47f029b2beb71dc48a0641e4e58
Nihal <nihal301298@gmail.com>
2025-01-06 23:34:02 +0530

    optimal solution

--- Basics/Hashing/SecondHeighOccurring.js
+++ Basics/Hashing/SecondHeighOccurring.js
@@ -7,6 +7,84 @@
 
 // brute 
 
+// class Solution {
+//     /* Function to get the second highest 
+//     occurring element in array */
+//     secondMostFrequentElement(nums) {
+        
+//         // Variable to store the size of array
+//         let n = nums.length;
+        
+//         /* Variable to store maximum frequency
+//         and second Max frequency */
+//         let maxFreq = 0;
+//         let secMaxFreq = 0;
+        
+//         /* Variable to store elements with most 
+//         and second most frequency */
+//         let maxEle = -1, secEle = -1;
+        
+//         // Visited array
+//         let visited = new Array(n).fill(false);
+        
+//         // First loop
+//         for (let i = 0; i < n; i++) {
+//             // Skip second loop if already visited
+//             if (visited[i]) continue;
+            
+//             /* Variable to store frequency
+//             of current element */
+//             let freq = 0;
+            
+//             // Second loop
+//             for (let j = i; j < n; j++) {
+//                 if (nums[i] === nums[j]) {
+//                     freq++;
+//                     visited[j] = true;
+//                 }
+//             }
+            
+//             /* Update variables if new element  
+//             having highest frequency or second
+//             highest frequency is found */
+//             if (freq > maxFreq) {
+//                 secMaxFreq = maxFreq;
+//                 maxFreq = freq;
+//                 secEle = maxEle;
+//                 maxEle = nums[i];
+//             } 
+//             else if (freq === maxFreq) {
+//                 maxEle = Math.min(maxEle, nums[i]);
+//             }
+//             else if (freq > secMaxFreq) {
+//                 secMaxFreq = freq;
+//                 secEle = nums[i];
+//             }
+//             else if (freq === secMaxFreq) {
+//                 secEle = Math.min(secEle, nums[i]);
+//             }
+//         }
+        
+//         // Return the result
+//         return secEle;
+//     }
+// }
+
+// // Example usage
+// let nums = [4, 4, 5, 5, 6, 7];
+
+// /* Creating an instance of 
+// Solution class */
+// let sol = new Solution();
+
+// /* Function call to get the second
+// highest occurring element in array */
+// let ans = sol.secondMostFrequentElement(nums);
+
+// console.log("The second highest occurring element in the array is: " + ans);
+
+// optimal solution 
+
 class Solution {
     /* Function to get the second highest 
     occurring element in array */
@@ -16,34 +94,25 @@ class Solution {
         let n = nums.length;
         
         /* Variable to store maximum frequency
-        and second Max frequency */
-        let maxFreq = 0;
-        let secMaxFreq = 0;
+        and second maximum frequency */
+        let maxFreq = 0, secMaxFreq = 0; 
         
-        /* Variable to store elements with most 
-        and second most frequency */
+        /* Variable to store element 
+        with maximum frequency and second
+        highest frequency */
         let maxEle = -1, secEle = -1;
         
-        // Visited array
-        let visited = new Array(n).fill(false);
+        // HashMap
+        let mpp = new Map();
         
-        // First loop
+        // Iterating on the array
         for (let i = 0; i < n; i++) {
-            // Skip second loop if already visited
-            if (visited[i]) continue;
+            // Updating hashmap 
+            mpp.set(nums[i], (mpp.get(nums[i]) || 0) + 1);
+        }
             
-            /* Variable to store frequency
-            of current element */
-            let freq = 0;
-            
-            // Second loop
-            for (let j = i; j < n; j++) {
-                if (nums[i] === nums[j]) {
-                    freq++;
-                    visited[j] = true;
-                }
-            }
-            
+        // Iterate on the map
+        for (let [ele, freq] of mpp) {
             /* Update variables if new element  
             having highest frequency or second
             highest frequency is found */
@@ -51,17 +120,17 @@ class Solution {
                 secMaxFreq = maxFreq;
                 maxFreq = freq;
                 secEle = maxEle;
-                maxEle = nums[i];
+                maxEle = ele;
             } 
-            else if (freq === maxFreq) {
-                maxEle = Math.min(maxEle, nums[i]);
+            else if (freq == maxFreq) {
+                maxEle = Math.min(maxEle, ele);
             }
             else if (freq > secMaxFreq) {
                 secMaxFreq = freq;
-                secEle = nums[i];
+                secEle = ele;
             }
-            else if (freq === secMaxFreq) {
-                secEle = Math.min(secEle, nums[i]);
+            else if (freq == secMaxFreq) {
+                secEle = Math.min(secEle, ele);
             }
         }
         
@@ -70,7 +139,7 @@ class Solution {
     }
 }
 
-// Example usage
+// Test the function
 let nums = [4, 4, 5, 5, 6, 7];
 
 /* Creating an instance of 
