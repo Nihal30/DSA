d28f902ba0b530e576d8a7750c36f6c1a05ae1f2
Nihal <nihal301298@gmail.com>
2025-01-08 22:52:29 +0530

    brute

---
+++ Basics/String/reverseString.js
@@ -0,0 +1,30 @@
+// Given a string, the task is to reverse it.
+//  The string is represented by an array of characters s. Perform the reversal in place with O(1) extra memory.
+
+// Input : s = ["h", "e" ,"l" ,"l" ,"o"]
+
+// Output : ["o", "l", "l", "e", "h"]
+
+// Explanation : The given string is s = "hello" and after reversing it becomes s = "olleh".
+
+// brute solution 
+
+function reverseString(s){
+   let reversed = []
+   for (let i=s.length - 1; i>=0 ; i--){
+    reversed.push(s[i])
+   }
+
+   for (let i =0;i<s.length;i++){
+    s[i]= reversed[i]
+   }
+
+   return s
+}
+
+let s = ['h', 'e', 'l', 'l', 'o'];
+reverseString(s);
+console.log(s); 
+
+// tc O(n)
+// sc O(n)
