cf190d35ea0f9a194436f151e28a99c1238b7987
Nihal <nihal301298@gmail.com>
2025-01-23 22:27:44 +0530

    optimal Moore Majority algo

--- Advance/Array/FAQ_Hard/MajorityElement1.js
+++ Advance/Array/FAQ_Hard/MajorityElement1.js
@@ -59,49 +59,102 @@
 // better ..
 // using hashing 
 
+// class Solution {
+//     // Function to find the majority element in an array
+//     majorityElement(nums) {
+        
+//         // Size of the given array
+//         let n = nums.length;
+        
+//         // Hash map to store element counts
+//         let map = new Map();
+        
+//         // Count occurrences of each element
+//         for (let num of nums) {
+//             map.set(num, (map.get(num) || 0) + 1);
+//         }
+        
+//         /* Iterate through the map to
+//         find the majority element */
+//         for (let [key, value] of map.entries()) {
+//             if (value > n / 2) {
+//                 return key;
+//             }
+//         }
+        
+//         // Return -1 if no majority element is found
+//         return -1;
+//     }
+// }
+
+// // Main function to test the Solution class
+// function main() {
+//     let arr = [2, 2, 1, 1, 1, 2, 2];
+    
+//     // Create an instance of Solution class
+//     let sol = new Solution();
+ 
+//     let ans = sol.majorityElement(arr);
+    
+//     // Print the majority element found
+//     console.log("The majority element is:", ans);
+// }
+
+// // Execute the main function
+// main();
+
+// optimal ..
+// Moore Majority Voting Algorithm
+
 class Solution {
     // Function to find the majority element in an array
     majorityElement(nums) {
-        
         // Size of the given array
         let n = nums.length;
         
-        // Hash map to store element counts
-        let map = new Map();
+        // Count
+        let cnt = 0;
         
-        // Count occurrences of each element
+        // Element
+        let el = 0;
+        
+        // Applying the algorithm
         for (let num of nums) {
-            map.set(num, (map.get(num) || 0) + 1);
-        }
-        
-        /* Iterate through the map to
-        find the majority element */
-        for (let [key, value] of map.entries()) {
-            if (value > n / 2) {
-                return key;
+            if (cnt === 0) {
+                cnt = 1;
+                el = num;
+            } else if (el === num) {
+                cnt++;
+            } else {
+                cnt--;
             }
         }
         
-        // Return -1 if no majority element is found
+        /* Checking if the stored element
+         is the majority element*/
+        let cnt1 = nums.filter(num => num === el).length;
+        
+        // Return element if it is a majority element
+        if (cnt1 > Math.floor(n / 2)) {
+            return el;
+        }
+        
+        // Return -1 if no such element found
         return -1;
     }
 }
 
-// Main function to test the Solution class
-function main() {
-    let arr = [2, 2, 1, 1, 1, 2, 2];
-    
-    // Create an instance of Solution class
-    let sol = new Solution();
- 
-    let ans = sol.majorityElement(arr);
-    
-    // Print the majority element found
-    console.log("The majority element is:", ans);
-}
+let arr = [2, 2, 1, 1, 1, 2, 2];
 
-// Execute the main function
-main();
+// Create an instance of Solution class
+let sol = new Solution();
+
+let ans = sol.majorityElement(arr);
+
+// Print the majority element found
+console.log(`The majority element is: ${ans}`);
+
+
 
 
 
