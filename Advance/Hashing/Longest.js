384b289fa0ea86ede0ef094a8fa82c8dbd47c6e8
Nihal <nihal301298@gmail.com>
2025-01-24 21:56:40 +0530

    optimal

--- Advance/Hashing/Longest.js
+++ Advance/Hashing/Longest.js
@@ -62,50 +62,99 @@
 // better
 
 
+// class Solution {
+//     longestConsecutive(nums) {
+//         let n = nums.length;
+
+//         // Return 0 if array is empty
+//         if (n === 0) return 0; 
+
+//         nums.sort((a, b) => a - b);
+
+//         // Track last smaller element
+//         let lastSmaller = -Infinity; 
+//         // Count current sequence length
+//         let cnt = 0; 
+//         // Track longest sequence length
+//         let longest = 1; 
+
+//         for (let i = 0; i < n; i++) {
+//             // If consecutive number exists
+//             if (nums[i] - 1 === lastSmaller) {
+//                 // Increment sequence count
+//                 cnt += 1; 
+//                 // Update last smaller element
+//                 lastSmaller = nums[i]; 
+//             } 
+//             // If consecutive number doesn't exist
+//             else if (nums[i] !== lastSmaller) {
+//                 // Reset count for new sequence
+//                 cnt = 1; 
+//                 // Update last smaller element
+//                 lastSmaller = nums[i]; 
+//             }
+//             // Update longest if needed
+//             longest = Math.max(longest, cnt); 
+//         }
+//         return longest;
+//     }
+// }
+
+// // Sample array
+// const a = [100, 4, 200, 1, 3, 2];
+
+// // Create an instance of solution class
+// const solution = new Solution(); 
+// // Function call for finding longest consecutive sequence
+// const ans = solution.longestConsecutive(a); 
+// console.log("The longest consecutive sequence is " + ans);
+
+// optimal
 class Solution {
     longestConsecutive(nums) {
         let n = nums.length;
+        // If the array is empty
+        if (n === 0) return 0;
 
-        // Return 0 if array is empty
-        if (n === 0) return 0; 
-
-        nums.sort((a, b) => a - b);
-
-        // Track last smaller element
-        let lastSmaller = -Infinity; 
-        // Count current sequence length
-        let cnt = 0; 
-        // Track longest sequence length
+        // Initialize the longest sequence length
         let longest = 1; 
+        let st = new Set();
 
+        // Put all the array elements into the set
         for (let i = 0; i < n; i++) {
-            // If consecutive number exists
-            if (nums[i] - 1 === lastSmaller) {
-                // Increment sequence count
-                cnt += 1; 
-                // Update last smaller element
-                lastSmaller = nums[i]; 
-            } 
-            // If consecutive number doesn't exist
-            else if (nums[i] !== lastSmaller) {
-                // Reset count for new sequence
-                cnt = 1; 
-                // Update last smaller element
-                lastSmaller = nums[i]; 
+            st.add(nums[i]);
+        }
+
+        // Traverse the set to find the longest sequence
+        for (let it of st) {
+            // Check if 'it' is a starting number of a sequence
+            if (!st.has(it - 1)) {
+                // Initialize the count of the current sequence
+                let cnt = 1; 
+                // Starting element of the sequence
+                let x = it; 
+
+                // Find consecutive numbers in the set
+                while (st.has(x + 1)) {
+                    // Move to the next element in the sequence
+                    x = x + 1; 
+                    // Increment the count of the sequence
+                    cnt = cnt + 1; 
+                }
+                // Update the longest sequence length
+                longest = Math.max(longest, cnt);
             }
-            // Update longest if needed
-            longest = Math.max(longest, cnt); 
         }
         return longest;
     }
 }
 
 // Sample array
-const a = [100, 4, 200, 1, 3, 2];
-
-// Create an instance of solution class
+const a = [100, 4, 200, 1, 3, 2]; 
+// Create an instance of the solution class
 const solution = new Solution(); 
-// Function call for finding longest consecutive sequence
+// Function call to find the longest consecutive sequence
 const ans = solution.longestConsecutive(a); 
 console.log("The longest consecutive sequence is " + ans);
+
 
