d12c57d4f6e7e5b975b28f1eb30c32c82eb210fa
Nihal <nihal301298@gmail.com>
2025-01-24 21:55:59 +0530

    brute / better

--- Advance/Hashing/Longest.js
+++ Advance/Hashing/Longest.js
@@ -7,54 +7,105 @@
 //  This sequence can be formed regardless of the initial order of the elements in the array.
 // brute 
 
+// class Solution {
+//     // Helper function to perform linear search
+//     linearSearch(nums, num) {
+//         const n = nums.length;
+//         // Traverse through the array
+//         for (let i = 0; i < n; i++) {
+//             if (nums[i] === num)
+//                 return true;
+//         }
+//         return false;
+//     }
+
+//     longestConsecutive(nums) {
+//         // If the array is empty
+//         if (nums.length === 0) {
+//             return 0;
+//         }
+//         const n = nums.length;
+//         // Initialize the longest sequence length
+//         let longest = 1;
+
+//         // Iterate through each element in the array
+//         for (let i = 0; i < n; i++) {
+//             // Current element
+//             let x = nums[i];
+//             // Count of the current sequence
+//             let cnt = 1;
+
+//             // Search for consecutive numbers
+//             while (this.linearSearch(nums, x + 1)) {
+//                 // Move to the next number in the sequence
+//                 x += 1;
+//                 // Increment the count of the sequence
+//                 cnt += 1;
+//             }
+
+//             // Update the longest sequence length found so far
+//             longest = Math.max(longest, cnt);
+//         }
+//         return longest;
+//     }
+// }
+
+// const a = [100, 4, 200, 1, 3, 2];
+
+// // Create an instance of the Solution class
+// const solution = new Solution();
+
+// // Function call for longest consecutive sequence
+// const ans = solution.longestConsecutive(a);
+// console.log("The longest consecutive sequence is", ans);
+
+// better
+
+
 class Solution {
-    // Helper function to perform linear search
-    linearSearch(nums, num) {
-        const n = nums.length;
-        // Traverse through the array
-        for (let i = 0; i < n; i++) {
-            if (nums[i] === num)
-                return true;
-        }
-        return false;
-    }
-
     longestConsecutive(nums) {
-        // If the array is empty
-        if (nums.length === 0) {
-            return 0;
-        }
-        const n = nums.length;
-        // Initialize the longest sequence length
-        let longest = 1;
+        let n = nums.length;
 
-        // Iterate through each element in the array
+        // Return 0 if array is empty
+        if (n === 0) return 0; 
+
+        nums.sort((a, b) => a - b);
+
+        // Track last smaller element
+        let lastSmaller = -Infinity; 
+        // Count current sequence length
+        let cnt = 0; 
+        // Track longest sequence length
+        let longest = 1; 
+
         for (let i = 0; i < n; i++) {
-            // Current element
-            let x = nums[i];
-            // Count of the current sequence
-            let cnt = 1;
-
-            // Search for consecutive numbers
-            while (this.linearSearch(nums, x + 1)) {
-                // Move to the next number in the sequence
-                x += 1;
-                // Increment the count of the sequence
-                cnt += 1;
+            // If consecutive number exists
+            if (nums[i] - 1 === lastSmaller) {
+                // Increment sequence count
+                cnt += 1; 
+                // Update last smaller element
+                lastSmaller = nums[i]; 
+            } 
+            // If consecutive number doesn't exist
+            else if (nums[i] !== lastSmaller) {
+                // Reset count for new sequence
+                cnt = 1; 
+                // Update last smaller element
+                lastSmaller = nums[i]; 
             }
-
-            // Update the longest sequence length found so far
-            longest = Math.max(longest, cnt);
+            // Update longest if needed
+            longest = Math.max(longest, cnt); 
         }
         return longest;
     }
 }
 
+// Sample array
 const a = [100, 4, 200, 1, 3, 2];
 
-// Create an instance of the Solution class
-const solution = new Solution();
+// Create an instance of solution class
+const solution = new Solution(); 
+// Function call for finding longest consecutive sequence
+const ans = solution.longestConsecutive(a); 
+console.log("The longest consecutive sequence is " + ans);
 
-// Function call for longest consecutive sequence
-const ans = solution.longestConsecutive(a);
-console.log("The longest consecutive sequence is", ans);
