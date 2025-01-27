93e06ed02b7aed79a57456d9443202001d948736
Nihal <nihal301298@gmail.com>
2025-01-27 22:03:41 +0530

    optimal

--- Advance/Hashing/SubarrayXor.js
+++ Advance/Hashing/SubarrayXor.js
@@ -76,3 +76,44 @@
 
 // console.log("The number of subarrays with XOR k is:", ans);
 
+// optimal 
+
+
+class Solution {
+    subarraysWithXorK(nums, k) {
+        const n = nums.length;
+        let xr = 0;
+        const mpp = new Map();
+        // setting the value of 0.
+        mpp.set(xr, (mpp.get(xr) || 0) + 1);
+        let cnt = 0;
+
+        for (let i = 0; i < n; i++) {
+            // prefix XOR till index i:
+            xr = xr ^ nums[i];
+
+            // By formula: x = xr ^ k:
+            const x = xr ^ k;
+
+            // add the occurrence of xr ^ k to the count:
+            cnt += mpp.get(x) || 0;
+
+            // Insert the prefix xor till index i into the map:
+            mpp.set(xr, (mpp.get(xr) || 0) + 1);
+        }
+        return cnt;
+    }
+}
+
+const a = [4, 2, 2, 6, 4];
+const k = 6;
+
+// Create an instance of the Solution class
+const solution = new Solution();
+
+// Function call to get the result
+const ans = solution.subarraysWithXorK(a, k);
+
+console.log("The number of subarrays with XOR k is:", ans);
+
+
