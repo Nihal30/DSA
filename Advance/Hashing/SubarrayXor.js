5d847e803b2cc73bd8ff334009ae87ff1406fa80
Nihal <nihal301298@gmail.com>
2025-01-27 21:57:13 +0530

    brute

---
+++ Advance/Hashing/SubarrayXor.js
@@ -0,0 +1,40 @@
+// Count subarrays with given xor K
+// Given an array of integers nums and an integer k, return the total number of subarrays whose XOR equals to k.
+// Input : nums = [4, 2, 2, 6, 4], k = 6
+// Output : 4
+// Explanation : The subarrays having XOR of their elements as 6 are [4, 2],  [4, 2, 2, 6, 4], [2, 2, 6], and [6]
+
+// brute 
+class Solution {
+    // Function to count the number of subarrays with XOR k
+    subarraysWithXorK(nums, k) {
+        const n = nums.length;
+        let cnt = 0;
+
+        // Step 1: Generate subarrays
+        for (let i = 0; i < n; i++) {
+            for (let j = i; j < n; j++) {
+                let xorr = 0;
+                /* Step 2: Calculate XOR of 
+                   all elements in the subarray */
+                for (let K = i; K <= j; K++) {
+                    xorr ^= nums[K];
+                }
+                // Step 3: Check XOR and count
+                if (xorr === k) cnt++;
+            }
+        }
+        return cnt;
+    }
+}
+
+const a = [4, 2, 2, 6, 4];
+const k = 6;
+
+// Create an instance of the Solution class
+const solution = new Solution();
+
+// Function call to get the result
+const ans = solution.subarraysWithXorK(a, k);
+
+console.log("The number of subarrays with XOR k is:", ans);
