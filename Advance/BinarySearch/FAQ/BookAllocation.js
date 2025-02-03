471abd3fb2498d8886c17b474420bb97e2f35949
Nihal <nihal301298@gmail.com>
2025-02-03 21:24:37 +0530

    bs

--- Advance/BinarySearch/FAQ/BookAllocation.js
+++ Advance/BinarySearch/FAQ/BookAllocation.js
@@ -72,3 +72,71 @@ function main() {
 
 // Call the main function
 main();
+
+class Solution {
+    /* Function to count the number of 
+    students required given the maximum 
+    pages each student can read */
+    countStudents(nums, pages) {
+        // Size of array
+        let n = nums.length;
+        
+        let students = 1;
+        let pagesStudent = 0;
+        
+        for (let i = 0; i < n; i++) {
+            if (pagesStudent + nums[i] <= pages) {
+                // Add pages to current student
+                pagesStudent += nums[i];
+            } else {
+                // Add pages to next student
+                students++;
+                pagesStudent = nums[i];
+            }
+        }
+        return students;
+    }
+
+    /* Function to allocate the book to ‘m’ 
+    students such that the maximum number 
+    of pages assigned to a student is minimum */
+    findPages(nums, m) {
+        // Size of array
+        let n = nums.length;
+        
+        // Book allocation impossible
+        if (m > n) return -1;
+
+        let low = Math.max(...nums);
+        let high = nums.reduce((a, b) => a + b, 0);
+
+        // Binary search for minimum maximum pages
+        while (low <= high) {
+            var mid = Math.floor((low + high) / 2);
+            var students = this.countStudents(nums, mid);
+            if (students > m) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
+
+function main() {
+    let nums = [25, 46, 28, 49, 24];
+    let m = 4;
+
+    // Create an instance of the Solution class
+    let sol = new Solution();
+
+    let ans = sol.findPages(nums, m);
+
+    // Output the result
+    console.log("The answer is:", ans);
+}
+
+// Call the main function
+main();
+
