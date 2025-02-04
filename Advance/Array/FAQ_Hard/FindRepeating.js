90eedcba46152dee7483cb0c71129b3d5078b788
Nihal <nihal301298@gmail.com>
2025-02-04 21:50:51 +0530

    better

--- Advance/Array/FAQ_Hard/FindRepeating.js
+++ Advance/Array/FAQ_Hard/FindRepeating.js
@@ -9,29 +9,78 @@
 // Explanation: 1 appears two times in the array and 2 is missing from nums
 
 // brute
+// class Solution {
+//     // Function to find repeating and missing numbers
+//     findMissingRepeatingNumbers(nums) {
+        
+//         // Size of the array
+//         let n = nums.length; 
+//         let repeating = -1, missing = -1;
+
+//         // Find the repeating and missing number:
+//         for (let i = 1; i <= n; i++) {
+            
+//             // Count the occurrences:
+//             let cnt = 0;
+//             for (let j = 0; j < n; j++) {
+//                 if (nums[j] === i) {
+//                     cnt++;
+//                 }
+//             }
+
+//             // Check if i is repeating or missing
+//             if (cnt === 2) {
+//                 repeating = i;
+//             } else if (cnt === 0) {
+//                 missing = i;
+//             }
+
+//             /* If both repeating and missing 
+//             are found, break out of loop*/
+//             if (repeating !== -1 && missing !== -1) {
+//                 break;
+//             }
+//         }
+
+//         // Return [repeating, missing]
+//         return [repeating, missing];
+//     }
+// }
+
+// const nums = [3, 1, 2, 5, 4, 6, 7, 5];
+
+// // Create an instance of Solution class
+// const sol = new Solution();
+
+// const result = sol.findMissingRepeatingNumbers(nums);
+
+// // Print the repeating and missing numbers found
+// console.log(`The repeating and missing numbers are: {${result[0]}, ${result[1]}}`);
+
+
+// better
 class Solution {
     // Function to find repeating and missing numbers
     findMissingRepeatingNumbers(nums) {
         
         // Size of the array
-        let n = nums.length; 
-        let repeating = -1, missing = -1;
+        let n = nums.length;
+        
+        // Hash array to count occurrences
+        let hash = Array(n + 1).fill(0);
+        
+        // Update the hash array:
+        for (let num of nums) {
+            hash[num]++;
+        }
 
+        let repeating = -1, missing = -1;
+        
         // Find the repeating and missing number:
         for (let i = 1; i <= n; i++) {
-            
-            // Count the occurrences:
-            let cnt = 0;
-            for (let j = 0; j < n; j++) {
-                if (nums[j] === i) {
-                    cnt++;
-                }
-            }
-
-            // Check if i is repeating or missing
-            if (cnt === 2) {
+            if (hash[i] === 2) {
                 repeating = i;
-            } else if (cnt === 0) {
+            } else if (hash[i] === 0) {
                 missing = i;
             }
 
@@ -41,12 +90,11 @@ class Solution {
                 break;
             }
         }
-
+        
         // Return [repeating, missing]
         return [repeating, missing];
     }
 }
-
 const nums = [3, 1, 2, 5, 4, 6, 7, 5];
 
 // Create an instance of Solution class
