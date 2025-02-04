5382a39cf4ba366f146ec22105ba57ab14543792
Nihal <nihal301298@gmail.com>
2025-02-04 21:47:21 +0530

    bs optimal

--- Advance/BinarySearch/2Darray/Search.js
+++ Advance/BinarySearch/2Darray/Search.js
@@ -46,41 +46,80 @@
 // console.log(result ? "true" : "false");
 
 // better using bs 
-class Solution {
-    // Function to perform binary search
-    binarySearch(nums, target) {
-        let n = nums.length;
-        let low = 0, high = n - 1;
+// class Solution {
+//     // Function to perform binary search
+//     binarySearch(nums, target) {
+//         let n = nums.length;
+//         let low = 0, high = n - 1;
 
-        // Perform binary search
-        while (low <= high) {
-            let mid = Math.floor((low + high) / 2);
+//         // Perform binary search
+//         while (low <= high) {
+//             let mid = Math.floor((low + high) / 2);
             
-            // Return true if target is found
-            if (nums[mid] === target) return true;
-            else if (target > nums[mid]) low = mid + 1;
-            else high = mid - 1;
-        }
-        // Return false if target not found
-        return false;
-    }
+//             // Return true if target is found
+//             if (nums[mid] === target) return true;
+//             else if (target > nums[mid]) low = mid + 1;
+//             else high = mid - 1;
+//         }
+//         // Return false if target not found
+//         return false;
+//     }
     
+//     // Function to search for a given target in matrix
+//     searchMatrix(mat, target) {
+//         let n = mat.length;
+//         let m = mat[0].length;
+
+//         for (let i = 0; i < n; i++) {
+            
+//             /* Check if there is a possibility that 
+//             the target can be found in current row*/
+//             if (mat[i][0] <= target && target <= mat[i][m - 1]) {
+                
+//                 /* Return result fetched 
+//                 from helper function*/
+//                 return this.binarySearch(mat[i], target);
+//             }
+//         }
+//         // Return false if target is not found
+//         return false; 
+//     }
+// }
+
+// let matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
+// let target = 8;
+
+// // Create an instance of Solution class
+// let sol = new Solution();
+
+// let result = sol.searchMatrix(matrix, target);
+
+// // Output the result
+// console.log(result ? "true" : "false");
+
+// Optimal 
+class Solution {
     // Function to search for a given target in matrix
     searchMatrix(mat, target) {
         let n = mat.length;
         let m = mat[0].length;
 
-        for (let i = 0; i < n; i++) {
+        let low = 0, high = n * m - 1;
+        
+        // Perform binary search
+        while (low <= high) {
+            let mid = Math.floor((low + high) / 2);
             
-            /* Check if there is a possibility that 
-            the target can be found in current row*/
-            if (mat[i][0] <= target && target <= mat[i][m - 1]) {
-                
-                /* Return result fetched 
-                from helper function*/
-                return this.binarySearch(mat[i], target);
-            }
+            // Calculate the row and column
+            let row = Math.floor(mid / m);
+            let col = mid % m;
+            
+            // If target is found return true
+            if (mat[row][col] === target) return true;
+            else if (mat[row][col] < target) low = mid + 1;
+            else high = mid - 1;
         }
+        
         // Return false if target is not found
         return false; 
     }
@@ -96,4 +135,5 @@ let result = sol.searchMatrix(matrix, target);
 
 // Output the result
 console.log(result ? "true" : "false");
+
 
