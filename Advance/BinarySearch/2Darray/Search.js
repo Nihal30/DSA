21dcf59f6c3ff18f3f6c45ce7166ac8a448720cc
Nihal <nihal301298@gmail.com>
2025-02-04 21:46:43 +0530

    BS

--- Advance/BinarySearch/2Darray/Search.js
+++ Advance/BinarySearch/2Darray/Search.js
@@ -8,25 +8,77 @@
 // Explanation: The target = 8 exists in the 'mat' at index (1, 3).
 
 // brute 
-class Solution {
+// class Solution {
     
-    searchMatrix(mat, target) {
-        // Check if the matrix is empty
-        if (mat.length === 0 || mat[0].length === 0) {
-            return false;
-        }
+//     searchMatrix(mat, target) {
+//         // Check if the matrix is empty
+//         if (mat.length === 0 || mat[0].length === 0) {
+//             return false;
+//         }
         
-        let n = mat.length;    
-        let m = mat[0].length; 
+//         let n = mat.length;    
+//         let m = mat[0].length; 
         
-        // Traverse the matrix
-        for (let i = 0; i < n; i++) {
-            for (let j = 0; j < m; j++) {
-                if (mat[i][j] === target) {
+//         // Traverse the matrix
+//         for (let i = 0; i < n; i++) {
+//             for (let j = 0; j < m; j++) {
+//                 if (mat[i][j] === target) {
                     
-                    // Return true if target is found
-                    return true;
-                }
+//                     // Return true if target is found
+//                     return true;
+//                 }
+//             }
+//         }
+//         // Return false if target is not found
+//         return false; 
+//     }
+// }
+
+// let matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
+// let target = 8;
+
+// // Create an instance of Solution class
+// let sol = new Solution();
+
+// let result = sol.searchMatrix(matrix, target);
+
+// // Output the result
+// console.log(result ? "true" : "false");
+
+// better using bs 
+class Solution {
+    // Function to perform binary search
+    binarySearch(nums, target) {
+        let n = nums.length;
+        let low = 0, high = n - 1;
+
+        // Perform binary search
+        while (low <= high) {
+            let mid = Math.floor((low + high) / 2);
+            
+            // Return true if target is found
+            if (nums[mid] === target) return true;
+            else if (target > nums[mid]) low = mid + 1;
+            else high = mid - 1;
+        }
+        // Return false if target not found
+        return false;
+    }
+    
+    // Function to search for a given target in matrix
+    searchMatrix(mat, target) {
+        let n = mat.length;
+        let m = mat[0].length;
+
+        for (let i = 0; i < n; i++) {
+            
+            /* Check if there is a possibility that 
+            the target can be found in current row*/
+            if (mat[i][0] <= target && target <= mat[i][m - 1]) {
+                
+                /* Return result fetched 
+                from helper function*/
+                return this.binarySearch(mat[i], target);
             }
         }
         // Return false if target is not found
@@ -44,3 +96,4 @@ let result = sol.searchMatrix(matrix, target);
 
 // Output the result
 console.log(result ? "true" : "false");
+
