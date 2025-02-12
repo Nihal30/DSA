83397667aaa2c8ccd11648a9650fb8b70b6fcb78
Nihal <nihal301298@gmail.com>
2025-02-12 21:26:15 +0530

    q

---
+++ Advance/Recursion/FAQ-H/Nqeen.js
@@ -0,0 +1,17 @@
+// N Queen
+// The challenge of arranging n queens on a n × n chessboard so that no two queens attack 
+// one another is known as the "n-queens puzzle."
+
+// Return every unique solution to the n-queens puzzle given an integer n.
+//  The answer can be returned in any sequence.
+
+// Every solution has a unique board arrangement for the placement of the n-queens,
+//  where 'Q' and '.' stand for a queen and an empty space, respectively.
+
+// Input : n = 4
+// Output : [[".Q.." , "...Q" , "Q..." , "..Q."] , ["..Q." , "Q..." , "...Q" , ".Q.."]]
+// Explanation : There are two possible combinations as shown below.
+
+// Input : n = 1
+// Output:
+// [["Q"]]
