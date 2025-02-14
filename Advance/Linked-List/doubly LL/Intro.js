95ccaa09ec6ddf17a53fa9c53b78c88a60bdf8ee
Nihal <nihal301298@gmail.com>
2025-02-14 21:09:28 +0530

    intro

---
+++ Advance/Linked-List/doubly LL/Intro.js
@@ -0,0 +1,11 @@
+class Node {
+    // Constructor to initialize a node with data
+    constructor(data) {
+        // Value of the node
+        this.data = data;        
+        // Pointer to the next node
+        this.next = null;      
+        // Pointer to the previous node
+        this.back = null;      
+    }
+}
