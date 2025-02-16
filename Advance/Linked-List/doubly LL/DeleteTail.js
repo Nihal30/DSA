4d7d4e79cc13bf5dc94274172cd94ca18f54edce
Nihal <nihal301298@gmail.com>
2025-02-16 11:17:13 +0530

    Q

---
+++ Advance/Linked-List/doubly LL/DeleteTail.js
@@ -0,0 +1,8 @@
+// Delete Tail of DLL
+
+// Given the head of a doubly linked list, remove the node at the tail of the linked list and return the head of the modified list.
+// The tail is the last node of the linked list.
+
+// Input: head -> 1 <-> 2 <-> 3
+// Output: head -> 1 <-> 2
+// Explanation: The node with value 3 was removed.
