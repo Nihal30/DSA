52cc4bc9a5c4cab53bdc1f9f191d8df87bc160bd
Nihal <nihal301298@gmail.com>
2025-02-19 21:14:01 +0530

    tc

--- Advance/Linked-List/logic/sort012.js
+++ Advance/Linked-List/logic/sort012.js
@@ -92,3 +92,10 @@ function newNode(data) {
     process.stdout.write("Sorted list: ");
     printList(head);
 })();
+
+// Complexity Analysis
+// Time Complexity: O(2N) because the code traverses the linked list twice: 
+// once while counting the frequency of 0's, 1's, and 2's, 
+// and once again while reassigning the values to the nodes. 
+// Here, N represents the length of the linked list or the number of nodes present in the linked list.
+// Space Complexity: O(1) because no extra space is used.
