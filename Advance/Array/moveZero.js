8c89de763ba3a5285e6911a63cdebfeaf591f578
Nihal <nihal301298@gmail.com>
2025-02-24 21:18:43 +0530

    tc

--- Advance/Array/moveZero.js
+++ Advance/Array/moveZero.js
@@ -90,6 +90,11 @@ let n = 10;
 let ans = moveZeros(n, arr);
 console.log(ans.join(' '));
 
+// Time Complexity: O(N), N = size of the array.
+// Reason: We have used 2 loops and using those loops, we are basically traversing the array once.
+
+// Space Complexity: O(1) as we are not using any extra space to solve this problem.
+
 
 
 
