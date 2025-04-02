50c578355556ee511e8af9295bdf0a0f406a569b
Nihal <nihal301298@gmail.com>
2025-04-02 23:16:59 +0530

    intro

---
+++ Advance/Bit manuplation/Into/intro.js
@@ -0,0 +1,17 @@
+
+
+// String vonver to Binary in CPP
+
+// string decimalToBinary(int n) {
+//     result = ""
+    
+//     while(n != 1) {
+//         if(n % 2 == 1) result += '1'
+//         else result += '0'
+        
+//         n = n / 2
+//     }
+    
+//     reverse(result)
+//     return result
+// }
