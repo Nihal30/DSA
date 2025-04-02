96e1b0d994cd1e35705558569ee52a0fa170c633
Nihal <nihal301298@gmail.com>
2025-04-02 23:28:56 +0530

    b2D

--- Advance/Bit manuplation/Into/intro.js
+++ Advance/Bit manuplation/Into/intro.js
@@ -19,3 +19,23 @@
 // Time Complexity: O(logN) Since the number is divided by 2 continuously.
 // Space Complexity: O(logN) Storing the bits.
 
+// Binary to Decimal Conversion
+// string binaryToDecimal(string str) {
+//     len = str.length
+//     val = 1, num = 0
+    
+//     for(i from len-1 to 0) {
+//         if(str[i] == '1') {
+//             num = num + val
+//         }
+        
+//         val = val * 2
+//     }
+    
+//     return num
+// }
+
+
+// Complexity Analysis:
+// Time Complexity: O(n) Traversing every bit in the string.
+// Space Complexity: O(1) Couple of variables used.
