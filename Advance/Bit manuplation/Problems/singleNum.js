68d01256dc8cd76895a564704353f8d2d7971d66
Nihal <nihal301298@gmail.com>
2025-04-04 23:43:23 +0530

    XOR

--- Advance/Bit manuplation/Problems/singleNum.js
+++ Advance/Bit manuplation/Problems/singleNum.js
@@ -1,47 +1,83 @@
-// Given an array of nums of n integers. Every integer in the array appears twice except one integer.
-// Find the number that appeared once in the array.
+// // Given an array of nums of n integers. Every integer in the array appears twice except one integer.
+// // Find the number that appeared once in the array.
 
-// Input : nums = [1, 2, 2, 4, 3, 1, 4]
-// Output : 3
-// Explanation : The integer 3 has appeared only once.
+// // Input : nums = [1, 2, 2, 4, 3, 1, 4]
+// // Output : 3
+// // Explanation : The integer 3 has appeared only once.
+
+// class Solution {
+//   /* Function to get the single 
+//     number in the given array */
+//   singleNumber(nums) {
+//     /* Map to store the elements 
+//         and their frequencies */
+//     let mpp = new Map();
+
+//     // Iterate on the array
+//     nums.forEach((num) => {
+//       mpp.set(num, (mpp.get(num) || 0) + 1); //Update the map
+//     });
+
+//     // Iterate on the map
+//     for (let [key, value] of mpp) {
+//       // If frequency is 1
+//       if (value === 1) {
+//         // Return the element
+//         return key;
+//       }
+//     }
+
+//     /* Return -1, if there is no 
+//         number having frequency 1 */
+//     return -1;
+//   }
+// }
+
+// // Example usage
+// let nums = [1, 2, 2, 4, 3, 1, 4];
+
+// /* Creating an instance of 
+// Solution class */
+// let sol = new Solution();
+
+// /* Function call to get the single 
+// number in the given array */
+// let ans = sol.singleNumber(nums);
+
+// console.log("The single number in given array is:", ans);
+
+// optimal using XOR
+
+
 
 class Solution {
-  /* Function to get the single 
+    /* Function to get the single 
     number in the given array */
-  singleNumber(nums) {
-    /* Map to store the elements 
-        and their frequencies */
-    let mpp = new Map();
-
-    // Iterate on the array
-    nums.forEach((num) => {
-      mpp.set(num, (mpp.get(num) || 0) + 1); //Update the map
-    });
-
-    // Iterate on the map
-    for (let [key, value] of mpp) {
-      // If frequency is 1
-      if (value === 1) {
-        // Return the element
-        return key;
-      }
+    singleNumber(nums) {
+        /* Variable to store XOR
+        of all numbers in array */
+        let XOR = 0;
+        
+        /* Iterate on the array to
+        find XOR of all elements */
+        for (let i = 0; i < nums.length; i++) {
+            XOR ^= nums[i];    
+        }
+        
+        // XOR stores the required answer
+        return XOR;        
     }
-
-    /* Return -1, if there is no 
-        number having frequency 1 */
-    return -1;
-  }
 }
 
-// Example usage
-let nums = [1, 2, 2, 4, 3, 1, 4];
+const nums = [1, 2, 2, 4, 3, 1, 4];
 
 /* Creating an instance of 
 Solution class */
-let sol = new Solution();
+const sol = new Solution();
 
 /* Function call to get the single 
 number in the given array */
-let ans = sol.singleNumber(nums);
+const ans = sol.singleNumber(nums);
 
-console.log("The single number in given array is:", ans);
+console.log("The single number in given array is: " + ans);
+
