1bff6dd46acff6deaa638d0445c725a5ccd4f0c5
Nihal <nihal301298@gmail.com>
2025-04-05 23:54:48 +0530

    brute

---
+++ Advance/Bit manuplation/Problems/2sNum.js
@@ -0,0 +1,53 @@
+// Given an array nums where each integer in nums appears thrice except one. Find out the number that has appeared only once.
+
+// Examples:
+// Input : nums = [2, 2, 2, 3]
+// Output : 3
+// Explanation : The integers 3 has appeared only once.
+
+
+// brute  using mpp
+class Solution {
+    /* Function to get the single 
+    number in the given array */
+    singleNumber(nums) {
+        
+        /* Map to store the elements 
+        and their frequencies */
+        let mpp = new Map();
+        
+        // Iterate on the array
+        for (let i = 0; i < nums.length; i++) {
+            mpp.set(nums[i], (mpp.get(nums[i]) || 0) + 1); // Update the map
+        }
+        
+        // Iterate on the map
+        for (let [key, value] of mpp.entries()) {
+            // If frequency is 1
+            if (value === 1) {
+                // Return the element
+                return key;
+            }
+        }
+        
+        /* Return -1, if there is no 
+        number having frequency 1 */
+        return -1;
+    }
+}
+
+// Test the function
+let nums = [2, 2, 2, 3];
+
+/* Creating an instance of 
+Solution class */
+let sol = new Solution();
+
+/* Function call to get the single 
+number in the given array */
+let ans = sol.singleNumber(nums);
+
+console.log("The single number in given array is: " + ans);
+
+
+
