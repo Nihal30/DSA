6556917457fab66fbd2de7a8814e428367d0995a
Nihal <nihal301298@gmail.com>
2025-04-16 00:08:30 +0530

    optimal

--- Pratice/removeDuplicates.js
+++ Pratice/removeDuplicates.js
@@ -1,38 +1,60 @@
-// Remove Duplicates From Sorted array
+// // Remove Duplicates From Sorted array
 
-class Solution {
-  removeAllDuplicates(nums) {
-    let set = new Set();
+// class Solution {
+//   removeAllDuplicates(nums) {
+//     let set = new Set();
 
-    for(let val of nums){
-        set.add(val)
+//     for(let val of nums){
+//         set.add(val)
+//     }
+
+//     let k = set.size;
+//     let j = 0;
+//     for(let val of s){
+//         nums[j++] = val;
+//     }
+//     return k
+//   }
+// }
+
+// // Helper function to print first n elements of the array
+// function printArray(nums, n) {
+//   for (let i = 0; i < n; i++) {
+//     process.stdout.write(nums[i] + " ");
+//   }
+//   console.log();
+// }
+
+// // Example usage
+// let nums = [1, 1, 2, 2, 2, 3, 3];
+
+// console.log("Original Array: ");
+// printArray(nums, nums.length);
+
+// // Create an instance of the Solution class
+// let sol = new Solution();
+
+// // Function call to remove duplicates from array
+// let k = sol.removeDuplicates(nums);
+
+
+
+// optimal 
+
+let nums =[1,1,2,2,3,3]
+
+function removeAllDuplicates (nums){
+    if(nums.length ===0){
+        return 0
     }
 
-    let k = set.size;
-    let j = 0;
-    for(let val of s){
-        nums[j++] = val;
+    let i=0;
+    for(let j=1;j<nums.length;j++){
+        if(nums[i] !==nums[j] ){
+            i++
+            nums[i]=nums[j]
+        }
     }
-    return k
-  }
+    return i+1
+
 }
-
-// Helper function to print first n elements of the array
-function printArray(nums, n) {
-  for (let i = 0; i < n; i++) {
-    process.stdout.write(nums[i] + " ");
-  }
-  console.log();
-}
-
-// Example usage
-let nums = [1, 1, 2, 2, 2, 3, 3];
-
-console.log("Original Array: ");
-printArray(nums, nums.length);
-
-// Create an instance of the Solution class
-let sol = new Solution();
-
-// Function call to remove duplicates from array
-let k = sol.removeDuplicates(nums);
