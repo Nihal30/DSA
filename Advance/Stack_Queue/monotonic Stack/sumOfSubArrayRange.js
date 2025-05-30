f10ce18408b69bc6c89c6be62feaa36e8b379b58
Nihal <nihal301298@gmail.com>
2025-05-30 22:41:20 +0530

    brute

--- Advance/Stack_Queue/monotonic Stack/sumOfSubArrayRange.js
+++ Advance/Stack_Queue/monotonic Stack/sumOfSubArrayRange.js
@@ -14,3 +14,59 @@
 // [2,3], range = 3 - 2 = 1
 // [1,2,3], range = 3 - 1 = 2
 // So the sum of all ranges is 0 + 0 + 0 + 1 + 1 + 2 = 4.
+
+// brute generating all the subarray and keep the track of smallest and largest
+
+class Solution {
+
+    /* Function to find the sum of 
+    subarray ranges in each subarray */
+    subArrayRanges(arr) {
+        
+        // Size of array
+        const n = arr.length;
+        
+        // To store the sum
+        let sum = 0;
+        
+        // Traverse on the array
+        for (let i = 0; i < n; i++) {
+            
+            // To store the smallest value of subarray
+            let smallest = arr[i];
+            
+            // To store the largest value of subarray
+            let largest = arr[i];
+            
+            /* Nested loop to get all 
+            subarrays starting from index i */
+            for (let j = i; j < n; j++) {
+                
+                // Update the smallest value
+                smallest = Math.min(smallest, arr[j]);
+                
+                // Update the largest value
+                largest = Math.max(largest, arr[j]);
+                
+                // Update the sum
+                sum += (largest - smallest);
+            }
+        }
+        
+        // Return the computed sum
+        return sum;
+    }
+}
+
+// Main function to test the solution
+const arr = [1, 2, 3];
+
+/* Creating an instance of 
+Solution class */
+const sol = new Solution(); 
+
+/* Function call to find the sum of 
+subarray ranges in each subarray */
+const ans = sol.subArrayRanges(arr);
+
+console.log("The sum of subarray ranges is:", ans);
