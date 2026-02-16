f5991d7741ef9a38864fb452ec7ac70e43d25afe
Nihal <nihal301298@gmail.com>
2026-02-16 21:48:17 +0530

    revision

--- Advance/Array/basic/maxConsecutive.js
+++ Advance/Array/basic/maxConsecutive.js
@@ -6,41 +6,65 @@
 // Output: 3
 // Explanation: The maximum consecutive 1s are present from index 4 to index 6, amounting to 3 1s
 
+// class Solution {
+//     findMaxConsecutiveOnes(nums) {
+//         /* Initialize count and max_count
+//               to track current and maximum consecutive 1s */
+//         let cnt = 0;
+//         let maxi = 0;
+
+//         // Traverse the array
+//         for (let i = 0; i < nums.length; i++) {
+
+//             /*If the current element is
+//            1, increment the count*/
+//             if (nums[i] == 1) {
+//                 cnt++;
+
+//                 /*Update maxi if current
+//                count is greater than maxi*/
+//                 maxi = Math.max(maxi, cnt);
+
+//             } else {
+//                 // If the current element is 0, reset the count
+//                 cnt = 0;
+//             }
+//         }
+
+//         // Return the maximum count of consecutive 1s
+//         return maxi;
+//     }
+// }
+
+// const nums = [1, 1, 0, 1, 1, 1];
+
+// // Create an instance of the Solution class
+// const sol = new Solution();
+
+// // Find and print the maximum consecutive 1s
+// const ans = sol.findMaxConsecutiveOnes(nums);
+// console.log("The maximum consecutive 1's are " + ans);
+
+// Revision -
+
 class Solution {
-    findMaxConsecutiveOnes(nums) {
-        /* Initialize count and max_count 
-              to track current and maximum consecutive 1s */
-        let cnt = 0;
-        let maxi = 0;
+  findMaxConsecutiveOnes(nums) {
+    let maxi = 0;
+    let cnt = 0;
 
-        // Traverse the array
-        for (let i = 0; i < nums.length; i++) {
-
-            /*If the current element is 
-           1, increment the count*/
-            if (nums[i] == 1) {
-                cnt++;
-
-                /*Update maxi if current 
-               count is greater than maxi*/
-                maxi = Math.max(maxi, cnt);
-
-            } else {
-                // If the current element is 0, reset the count
-                cnt = 0;
-            }
-        }
-
-        // Return the maximum count of consecutive 1s
-        return maxi;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] == 1) {
+        cnt++;
+        maxi = Math.max(maxi, cnt);
+      } else {
+        cnt = 0;
+      }
     }
+    return maxi;
+  }
 }
 
 const nums = [1, 1, 0, 1, 1, 1];
-
-// Create an instance of the Solution class
 const sol = new Solution();
-
-// Find and print the maximum consecutive 1s
 const ans = sol.findMaxConsecutiveOnes(nums);
-console.log("The maximum consecutive 1's are " + ans);
+console.log("max", ans);
